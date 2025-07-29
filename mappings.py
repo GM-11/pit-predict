403c0ad797a5bbd7cc7b6cf2ac865d62e8263d6d
GM-11 <mathuyrgopal@gmail.com>
2025-07-30 00:17:12 +0530

    Add F1 compound, driver, and track mappings

---
+++ mappings.py
@@ -0,0 +1,96 @@
+compound_mapping = {
+    0: "HARD TYRES",
+    1: "HYPERSOFT TYRES",
+    2: "INTERMEDIATE TYRES",
+    3: "MEDIUM TYRES",
+    4: "SOFT TYRES",
+    5: "SUPERSOFT TYRES",
+    6: "ULTRASOFT TYRES",
+    7: "WET TYRES",
+}
+
+driver_mapping = {
+    0: "Jack Aitken",  # AIT
+    1: "Alex Albon",
+    2: "Fernando Alonso",
+    3: "Kimi Antonelli",  # ANT
+    4: "Oliver Bearman",
+    5: "Gabriel Bortoleto",  # BOR
+    6: "Valtteri Bottas",  # BOT
+    7: "Franco Colapinto",
+    8: "Nyck de Vries",  # DEV
+    9: "Jack Doohan",
+    10: "Marcus Ericsson",  # ERI
+    11: "Enzo Fittipaldi",  # FIT
+    12: "Pierre Gasly",
+    13: "Antonio Giovinazzi",  # GIO
+    14: "Romain Grosjean",  # GRO
+    15: "Isack Hadjar",
+    16: "Lewis Hamilton",
+    17: "Luca Ghiotto",  # HAR (tentative, still unclear)
+    18: "Nico Hülkenberg",
+    19: "Robert Kubica",  # KUB
+    20: "Daniil Kvyat",  # KVY
+    21: "Nicholas Latifi",  # LAT
+    22: "Liam Lawson",
+    23: "Charles Leclerc",
+    24: "Kevin Magnussen",  # MAG
+    25: "Nikita Mazepin",  # MAZ
+    26: "Mick Schumacher",  # MSC
+    27: "Lando Norris",
+    28: "Esteban Ocon",
+    29: "Sergio Pérez",  # PER
+    30: "Oscar Piastri",
+    31: "Kimi Räikkönen",  # RAI
+    32: "Daniel Ricciardo",
+    33: "George Russell",
+    34: "Carlos Sainz",
+    35: "Logan Sargeant",  # SAR
+    36: "Sergey Sirotkin",  # SIR
+    37: "Lance Stroll",  # STR
+    38: "Yuki Tsunoda",
+    39: "Stoffel Vandoorne",  # VAN
+    40: "Max Verstappen",
+    41: "Sebastian Vettel",  # VET
+    42: "Guanyu Zhou",  # ZHO
+}
+
+
+track_mapping = {
+    0: "70th Anniversary Grand Prix",
+    1: "Abu Dhabi Grand Prix",
+    2: "Australian Grand Prix",
+    3: "Austrian Grand Prix",
+    4: "Azerbaijan Grand Prix",
+    5: "Bahrain Grand Prix",
+    6: "Belgian Grand Prix",
+    7: "Brazilian Grand Prix",
+    8: "British Grand Prix",
+    9: "Canadian Grand Prix",
+    10: "Chinese Grand Prix",
+    11: "Dutch Grand Prix",
+    12: "Eifel Grand Prix",
+    13: "Emilia Romagna Grand Prix",
+    14: "French Grand Prix",
+    15: "German Grand Prix",
+    16: "Hungarian Grand Prix",
+    17: "Italian Grand Prix",
+    18: "Japanese Grand Prix",
+    19: "Las Vegas Grand Prix",
+    20: "Mexican Grand Prix",
+    21: "Mexico City Grand Prix",
+    22: "Miami Grand Prix",
+    23: "Monaco Grand Prix",
+    24: "Portuguese Grand Prix",
+    25: "Qatar Grand Prix",
+    26: "Russian Grand Prix",
+    27: "Sakhir Grand Prix",
+    28: "Saudi Arabian Grand Prix",
+    29: "Singapore Grand Prix",
+    30: "Spanish Grand Prix",
+    31: "Styrian Grand Prix",
+    32: "São Paulo Grand Prix",
+    33: "Turkish Grand Prix",
+    34: "Tuscan Grand Prix",
+    35: "United States Grand Prix",
+}
